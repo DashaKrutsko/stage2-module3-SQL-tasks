a984e1736d9b9ec5b1c7d8c5b5156aa2d278a309
DK <dasha.krutsko@gmail.com>
2023-01-05 16:30:47 +0300

    update

--- lesson8/src/main/resources/SOLUTION.sql
+++ lesson8/src/main/resources/SOLUTION.sql
@@ -1,5 +1,4 @@
-SELECT min(birthday) FROM student;
+SELECT max(birthday) FROM student;
 SELECT min(payment_date) FROM payment;
-SELECT avg(mark.mark) FROM mark;
-SELECT min(AMOUNT) FROM payment LEFT JOIN paymenttype P2 on P2.id = payment.type_id WHERE p2.name='WEEKLY';
-
+SELECT avg(mark.mark) FROM mark JOIN subject ON mark.subject_id = subject.id WHERE subject.name = 'Math';
+SELECT min(AMOUNT) FROM payment JOIN paymenttype P2 on P2.id = payment.type_id WHERE p2.name='WEEKLY';
